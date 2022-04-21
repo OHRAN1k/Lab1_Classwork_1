34016d3779fa66f6a34866692aefac6701337a19
OHRAN1k <dima.dmitriev17.02@gmail.com>
2022-04-21 22:22:34 +0300

    Lab1 classwork

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ sides = sorted(sides, reverse=True)
 
 smax = 0
 for i in range(len(sides)):
-     for j in range(i + 1, len(sides)):
+    for j in range(i + 1, len(sides)):
         for k in range(j + 1, len(sides)):
-             a = sides[i]
-             b = sides[j]
-             c = sides[k]
-                    if a + b > c and a + c > b and b + c > a:
-                    p = (a + b + c) / 2
-                    s = (p * (p - a) * (p - b) * (p - c)) ** (1 / 2)
-                    if s > smax:
+            a = sides[i]
+            b = sides[j]
+            c = sides[k]
+            if a + b > c and a + c > b and b + c > a:
+                p = (a + b + c) / 2
+                s = (p * (p - a) * (p - b) * (p - c)) ** (1 / 2)
+                if s > smax:
                     smax = s
- print("Максимальная площадь треугольника", smax)
+                    print("Максимальная площадь треугольника", smax)
